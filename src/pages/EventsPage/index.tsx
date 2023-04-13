07efdaf27ee4cc14f72fd6610b4c8058f71dcef9
DejanTeofilovic <dejanteofilovic2@gmail.com>
2023-04-14 02:34:17 +0900

    Upcoming Soon

--- src/pages/EventsPage/index.tsx
+++ src/pages/EventsPage/index.tsx
@@ -1,5 +1,14 @@
+import React from "react";
+import Container from "../../components/Container";
+
+// -----------------------------------------------------------------------------
+
 export default function EventsPage() {
   return (
-    <div></div>
+    <div>
+      <Container>
+        <p className="text-3xl md:text-5xl font-bold text-center my-16 md:my-32">Upcoming Soon...</p>
+      </Container>
+    </div>
   )
 }
--- src/pages/LearnPage/index.tsx
+++ src/pages/LearnPage/index.tsx
@@ -1,4 +1,4 @@
-import { lazy } from 'react'
+import React, { lazy } from 'react'
 import Container from '../../components/Container'
 
 // -----------------------------------------------------------------------
