210d7fa3e4b0c95b952bdbfeede90b744e8989b5
DejanTeofilovic <dejanteofilovic2@gmail.com>
2023-04-14 10:21:36 +0900

    alert-remove

--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -15,17 +15,6 @@ const TestimonialsSection = lazy(() => import('./TestimonialsSection'))
 export default function HomePage() {
   return (
     <div className="flex flex-col gap-16 md:gap-36">
-      <Container>
-        <p className='text-3xl font-bold'>
-          Hello. My freelancer account has been closed so i can't continue the chat with you in freelancer.com. :(
-          If you are interested in keeping contract with me, please reply me by one of the following platforms.<br />
-
-          Email: mr.new0509@gmail.com<br />
-          Telegram: @mrnew0509<br />
-          discord: OpeThink#5316<br />
-          skype: sin085
-        </p>
-      </Container>
       <HeroSection />
       <AboutSection />
       <ValuesSection />
