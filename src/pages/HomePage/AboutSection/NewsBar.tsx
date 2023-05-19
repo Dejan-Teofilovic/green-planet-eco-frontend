53d0863b8f5a3fce578939ab0f2ae98255c9fa90
DejanTeofilovic <dejanteofilovic2@gmail.com>
2023-05-19 17:29:13 +0900

    2 logos are added

--- src/pages/HomePage/AboutSection/NewsBar.tsx
+++ src/pages/HomePage/AboutSection/NewsBar.tsx
@@ -5,15 +5,21 @@ export default function NewsBar() {
   return (
     <div>
       <Marquee direction="right">
-        <div className="flex items-center gap-16 md:gap-60">
+        <div className="flex items-center gap-16 md:gap-48">
           <a href="https://www.newsbtc.com/press-releases/green-planet-eco-the-future-is-calling-us-join-our-pre-sale-today/" target="_blank">
-            <img src="/assets/images/news_logo_btc.png" alt="news logo" className="w-48" />
+            <img src="/assets/images/news_logo_btc.png" alt="news btc" className="w-48" />
           </a>
           <a href="https://icolink.com/ico-green-planet-eco.html" target="_blank">
-            <img src="/assets/images/news_logo_iconlink.png" alt="news logo" className="w-32" />
+            <img src="/assets/images/news_logo_iconlink.png" alt="iconlink" className="w-32" />
           </a>
-          <a href="https://coinsniper.net/coin/47865" target="_blank" className="pr-16 md:pr-60">
-            <img src="/assets/images/news_logo_coinsniper.png" alt="news logo" className="w-56" />
+          <a href="https://coinsniper.net/coin/47865" target="_blank">
+            <img src="/assets/images/news_logo_coinsniper.png" alt="coinsniper" className="w-56" />
+          </a>
+          <a href="https://medium.com/@giving_flirt_porcupine_407/green-planet-eco-the-future-is-calling-us-join-our-pre-sale-today-2c15414885ec" target="_blank">
+            <img src="/assets/images/news_logo_medium.png" alt="medium" className="w-56" />
+          </a>
+          <a href="https://steemit.com/cryptocurrency/@greenplaneteco/4xkdrs-green-planet-eco-the-future-is-calling-us-join-our-pre-sale-today" target="_blank" className="pr-16 md:pr-48">
+            <img src="/assets/images/news_logo_steemit.png" alt="steemit" className="w-56" />
           </a>
         </div>
       </Marquee>
