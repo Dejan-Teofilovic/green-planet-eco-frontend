e33afcfb7833ad52ab71bd6779a9cec4e1512c3b
DejanTeofilovic <dejanteofilovic2@gmail.com>
2023-06-25 14:14:21 +0900

    icoholder news added

--- src/pages/HomePage/AboutSection/NewsBar.tsx
+++ src/pages/HomePage/AboutSection/NewsBar.tsx
@@ -24,8 +24,11 @@ export default function NewsBar() {
           <a href="https://coincodex.com/ico/green-planet-eco" target="_blank">
             <img src="/assets/images/news_logo_coincodex.png" alt="coincodex" className="w-56" />
           </a>
-          <a href="https://coinpedia.org/press-release/green-planet-eco-the-future-is-calling-us-join-our-pre-sale-today/" target="_blank" className="pr-16 md:pr-48">
+          <a href="https://coinpedia.org/press-release/green-planet-eco-the-future-is-calling-us-join-our-pre-sale-today/" target="_blank">
             <img src="/assets/images/news_logo_coinpedia.png" alt="coinpedia" className="w-48" />
+          </a>
+          <a href="https://icoholder.com/en/interview/make-the-earth-better-with-green-planet-eco" target="_blank" className="pr-16 md:pr-48">
+            <img src="/assets/images/news_logo_icoholder.png" alt="coinpedia" className="w-48" />
           </a>
         </div>
       </Marquee>
