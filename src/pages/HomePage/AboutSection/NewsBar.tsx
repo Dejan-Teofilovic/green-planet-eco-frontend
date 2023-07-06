473919b8e2d8dad738ad08f8811e44fa14cc9847
DejanTeofilovic <dejanteofilovic2@gmail.com>
2023-07-06 22:54:35 +0900

    cointelegraph added

--- src/pages/HomePage/AboutSection/NewsBar.tsx
+++ src/pages/HomePage/AboutSection/NewsBar.tsx
@@ -5,9 +5,9 @@ export default function NewsBar() {
   return (
     <div>
       <Marquee direction="right">
-        <div className="flex items-center gap-16 md:gap-48">
+        <div className="flex items-center gap-16 md:gap-24">
           <a href="https://www.newsbtc.com/press-releases/green-planet-eco-the-future-is-calling-us-join-our-pre-sale-today/" target="_blank">
-            <img src="/assets/images/news_logo_btc.png" alt="news btc" className="w-48" />
+            <img src="/assets/images/news_logo_btc.png" alt="news btc" className="w-56" />
           </a>
           <a href="https://icolink.com/ico-green-planet-eco.html" target="_blank">
             <img src="/assets/images/news_logo_iconlink.png" alt="iconlink" className="w-32" />
@@ -27,8 +27,11 @@ export default function NewsBar() {
           <a href="https://coinpedia.org/press-release/green-planet-eco-the-future-is-calling-us-join-our-pre-sale-today/" target="_blank">
             <img src="/assets/images/news_logo_coinpedia.png" alt="coinpedia" className="w-48" />
           </a>
-          <a href="https://icoholder.com/en/interview/make-the-earth-better-with-green-planet-eco" target="_blank" className="pr-16 md:pr-48">
-            <img src="/assets/images/news_logo_icoholder.png" alt="coinpedia" className="w-48" />
+          <a href="https://cointelegraph.com/press-releases/green-planet-announces-eco-token-listing-on-p2b-crypto-exchange" target="_blank">
+            <img src="/assets/images/news_logo_cointelegraph.png" alt="cointelegraph" className="w-64" />
+          </a>
+          <a href="https://icoholder.com/en/interview/make-the-earth-better-with-green-planet-eco" target="_blank" className="pr-16 md:pr-24">
+            <img src="/assets/images/news_logo_icoholder.png" alt="icoholder" className="w-48" />
           </a>
         </div>
       </Marquee>
