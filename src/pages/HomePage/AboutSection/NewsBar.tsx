7a23ec30b0900c10e08a8b40bafa72f36d9f45ee
DejanTeofilovic <dejanteofilovic2@gmail.com>
2023-07-14 21:11:57 +0900

    OnePlanet news added

--- src/pages/HomePage/AboutSection/NewsBar.tsx
+++ src/pages/HomePage/AboutSection/NewsBar.tsx
@@ -4,7 +4,7 @@ import Marquee from "react-fast-marquee"
 export default function NewsBar() {
   return (
     <div>
-      <Marquee direction="right">
+      <Marquee direction="right" speed={100}>
         <div className="flex items-center gap-16 md:gap-24">
           <a href="https://www.newsbtc.com/press-releases/green-planet-eco-the-future-is-calling-us-join-our-pre-sale-today/" target="_blank">
             <img src="/assets/images/news_logo_btc.png" alt="news btc" className="w-56" />
@@ -29,6 +29,9 @@ export default function NewsBar() {
           </a>
           <a href="https://cointelegraph.com/press-releases/green-planet-announces-eco-token-listing-on-p2b-crypto-exchange" target="_blank">
             <img src="/assets/images/news_logo_cointelegraph.png" alt="cointelegraph" className="w-64" />
+          </a>
+          <a href="https://www.oneplanetnetwork.org/members-directory/member/17091" target="_blank">
+            <img src="/assets/images/news_logo_oneplanet.png" alt="One Planet" className="w-72" />
           </a>
           <a href="https://icoholder.com/en/interview/make-the-earth-better-with-green-planet-eco" target="_blank" className="pr-16 md:pr-24">
             <img src="/assets/images/news_logo_icoholder.png" alt="icoholder" className="w-48" />
